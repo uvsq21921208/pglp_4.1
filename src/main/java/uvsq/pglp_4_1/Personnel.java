e34579d7feb25a2ec147a3a9aa814309f0d32535
uvsq21921208 <mouttiedje@outlook.com>
2020-03-04 12:06:29 +0100

    Builder implementation.

---
+++ src/main/java/uvsq/pglp_4_1/Personnel.java
@@ -0,0 +1,81 @@
+package uvsq.pglp_4_1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Mouttie Classe personnel.
+ */
+public final class Personnel {
+	/**
+	 * Le nom d'un personnel.
+	 */
+	private String nom;
+	/**
+	 * Le prenom d'un personnel.
+	 */
+	private String prenom;
+	/**
+	 * Le numero du telephone.
+	 */
+	private ArrayList<String> telephones;
+	/**
+	 * date de naissance.
+	 */
+	private LocalDate dataDeNaissance;
+	/**
+	 * Fonction.
+	 */
+	private String fonction;
+
+	public static class PersonelBuilder {
+		/**
+		 * Le nom d'un personnel.
+		 */
+		private String nom;
+		/**
+		 * Le prenom d'un personnel.
+		 */
+		private String prenom;
+		/**
+		 * Le numero du telephone.
+		 */
+		private ArrayList<String> telephones = null;
+		/**
+		 * date de naissance.
+		 */
+		private LocalDate dataDeNaissance = null;
+		/**
+		 * Fonction.
+		 */
+		private String fonction;
+		public PersonelBuilder(String nom, String prenom, String fonction) {
+			this.nom = nom;
+			this.prenom = prenom;
+			this.fonction = fonction;
+		}
+		public PersonelBuilder dateNaissance(LocalDate date) {
+		    this.dataDeNaissance = date;
+			return this;
+		}
+		
+		public PersonelBuilder telephone(String phone) {
+			this.telephones = new ArrayList<String>();
+			this.telephones.add(phone);
+			return this;
+		}
+		public Personnel build() {
+			return new Personnel(this);
+		}
+
+	}
+
+	private Personnel(PersonelBuilder builder) {
+		this.nom = builder.nom;
+		this.prenom = builder.prenom;
+		this.fonction = builder.fonction;
+	}
+
+
+}
